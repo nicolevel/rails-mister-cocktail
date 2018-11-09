c6c38423d25b2c44dc19693a2ef1fe6961bf9857
Nicole Velasco <nicolevelasco455@gmail.com>
2018-11-09 16:11:07 -0300

    photo uploader

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,9 +14,9 @@ class CocktailsController < ApplicationController
   end
 
   def create        # POST /restaurants
-    @cocktails = Cocktail.new(cocktail_params)
-    if @cocktails.save
-      redirect_to cocktails_path
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
